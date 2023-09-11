cff537a3eef927e51b788301acf0014904aa82cf
wai-1002 <wai.create2021@gmail.com>
2023-09-11 17:18:40 +0900

    init

--- GrammarPractice.playground/Contents.swift
+++ GrammarPractice.playground/Contents.swift
@@ -488,3 +488,58 @@ if let user = User(json: json) {
 } else {
     print("Invalid JSON")
 }
+
+//オブザーバパターン
+class Poster {
+    static let notificationName = Notification.Name("SomeNotification")
+    
+    func post() {
+        NotificationCenter.default.post(
+            name: Poster.notificationName, object: nil)
+    }
+}
+
+import Dispatch
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let queue = DispatchQueue.global(pos: .userInitiated)
+queue.async {
+    Thread.isMainThread
+    print("非同期の処理")
+}
+
+//エラー処理
+struct User2 {
+    let id: Int
+    let name: String
+    let email: String
+    
+}
+
+func findUser(byID id: Int) -> User2? {
+    let users = [
+        User2(id: 1,
+              name: "Yusei Nishiyama",
+             email: "nishiyama@example.com"),
+        User2(id: 2,
+              name: "Yosuke Ishikawa",
+              email: "ishikawa@example.com")
+        ]
+    
+    for user in users {
+        if user.id == id {
+            return user
+        }
+    }
+    
+    return nil
+}
+
+let id = 1
+if let user = findUser(byID: id) {
+    print("Name: \(user.name)")
+} else {
+    print("Error: User not found")
+}
